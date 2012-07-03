e84ea816d9e197c0e83d1f9cf818f4bbc738c616
abhishek <abhishekdagarit@gmail.com>
2012-07-03 19:30:46 +0530

    cart created successfully

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 class Product < ActiveRecord::Base
 attr_accessible :description, :image_url, :price, :title
+ has_many :line_items
+ before_destroy :ensure_not_referenced_by_any_line_item
+ 
+ private
+ def ensure_not_referenced_by_any_line_item
+  if line_items.empty?
+    return true
+  else
+    errors.add(:base, 'Line Items present')
+    return false
+   end
+ end
  default_scope :order => 'title' 
  validates :title, :description, :image_url, :price, :presence => true
  validates :price, :numericality => {:less_than => 5000, :greater_than => 0.01 }
